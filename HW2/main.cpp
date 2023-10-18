f6a985b8946f25603585299462bc28d5e1a06dd7
Larry_CNS <larrykuo.tech@gmail.com>
2023-10-18 23:04:51 +0800

    edit ListScheduling() 1018

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -8,30 +8,6 @@
 # include <functional>
 using namespace std ;
 
-unordered_map<string,size_t> gGateVertex ;
-unordered_map<string,vector<string>> gGateInbound ;
-unordered_map<string,bool> gGateStatus ;
-queue<string> readyqueue ;
-string gModelName ;
-string gNewToken ;
-vector<string> gInputs ;
-vector<string> gOutputs ;
-int gandConstraint = 0;
-int gorConstraint = 0;
-int gnotConstraint = 0;
-string gblifFile ;
-
-void readLabel(ifstream & file) ;
-void analyzeLabel( string type, ifstream & file ) ;
-void readUntilNotWhiteSpace( ifstream & file) ;
-vector<string> readGate(ifstream & file ) ;
-string GetaToken(ifstream & file) ;
-string ReadaToken( ifstream & file ) ;
-string PeekaToken(ifstream & file) ;
-bool IsNumber( char ch ) ;
-string analyzeGate(ifstream & file, vector<string> gate) ;
-void buildTree( string type, vector<string> gate ) ;
-
 struct VertexProperty {
     string name;
     string type;
@@ -42,13 +18,43 @@ struct ReadyQueue {
     int delay ;
 } ;
 
+string gModelName ;
+string gNewToken ;
+set<string> gInputs ;
+set<string> gOutputs ;
+int gandConstraint = 0;
+int gorConstraint = 0;
+int gnotConstraint = 0;
+string gblifFile ;
+
+// List Scheduling
+unordered_map<string,vector<string>> gGateInbound ;
+unordered_map<string,bool> gGateStatus ;
+queue<string> ANDreadyqueue ;
+queue<string> ORreadyqueue ;
+queue<string> NOTreadyqueue ;
+// queue<string> waitingqueue ; // wait for the resource
+
+
 // Tree
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, VertexProperty> DirectedGraph;
 typedef boost::graph_traits<DirectedGraph>::vertex_descriptor vertex_descriptor_t;
 DirectedGraph g;
 typedef DirectedGraph::vertex_descriptor Vertex;
-vertex_descriptor_t src, dst;
+vertex_descriptor_t ghead, gtail ;
+unordered_map<string,size_t> gGateVertex ;
 // 
+
+void readLabel(ifstream & file) ;
+void analyzeLabel( string type, ifstream & file ) ;
+void readUntilNotWhiteSpace( ifstream & file) ;
+set<string> readGate(ifstream & file ) ;
+string GetaToken(ifstream & file) ;
+string ReadaToken( ifstream & file ) ;
+string PeekaToken(ifstream & file) ;
+bool IsNumber( char ch ) ;
+string analyzeGate(ifstream & file, vector<string> gate) ;
+void buildTree( string type, vector<string> gate ) ;
 
 void readFile() {
     ifstream inputFile( "./aoi_benchmark/"+ gblifFile ) ;
@@ -124,18 +130,18 @@ void readLabel( ifstream & file ) {
 } // readLabel
 
 void analyzeLabel( string type, ifstream & file ) {
-    vertex_descriptor_t head, tail ;
+    // vertex_descriptor_t head, tail ;
     if ( ! gGateVertex["head"] && ! gGateVertex["tail"] ) {
-        head = boost::add_vertex(g);
-        tail = boost::add_vertex(g);
-        g[head].name = "head" ;
-        g[tail].name = "tail" ;
-        gGateVertex["head"] = head ;
-        gGateVertex["tail"] = tail ;
+        ghead = boost::add_vertex(g);
+        gtail = boost::add_vertex(g);
+        g[ghead].name = "head" ;
+        g[gtail].name = "tail" ;
+        gGateVertex["head"] = ghead ;
+        gGateVertex["tail"] = gtail ;
     } // if
     else {
-        head = gGateVertex["head"] ;
-        tail = gGateVertex["tail"] ;
+        ghead = gGateVertex["head"] ;
+        gtail = gGateVertex["tail"] ;
     }
     cout << "analyzeLabel " << type << endl ;
     if ( type == ".model") {
@@ -147,12 +153,13 @@ void analyzeLabel( string type, ifstream & file ) {
         gInputs = readGate(file) ;
 
         for ( auto i : gInputs ) {
-            cout << i << " " ;
-            DirectedGraph::vertex_descriptor temp_vertex = boost::add_vertex(g);
-            gGateVertex[i] = temp_vertex ;
-            g[temp_vertex].name = i ;
-            g[temp_vertex].type = "NOP" ;
-            boost::add_edge(head, temp_vertex, g);
+            // DirectedGraph::vertex_descriptor temp_vertex = boost::add_vertex(g);
+            // gGateVertex[i] = temp_vertex ;
+            // g[temp_vertex].name = i ;
+            // g[temp_vertex].type = "NOP" ;
+            // boost::add_edge(ghead, temp_vertex, g);
+            
+            gGateStatus[i] = true ;
         }
 
         cout << endl ;
@@ -162,24 +169,28 @@ void analyzeLabel( string type, ifstream & file ) {
 
         cout << "\toutputs: " ;
         for ( auto i : gOutputs ) {
-            DirectedGraph::vertex_descriptor temp_vertex = boost::add_vertex(g);
-            gGateVertex[i] = temp_vertex ;
-            g[temp_vertex].name = i ;
-            g[temp_vertex].type = "NOP" ;
-            boost::add_edge(temp_vertex, tail, g);
+            // DirectedGraph::vertex_descriptor temp_vertex = boost::add_vertex(g);
+            // gGateVertex[i] = temp_vertex ;
+            // g[temp_vertex].name = i ;
+            // g[temp_vertex].type = "NOP" ;
+            // boost::add_edge(temp_vertex, gtail, g);
+
+            gGateStatus[i] = false ;
         } // for
         // cout << endl ;
     }
     else if ( type == ".names") {
         cout << "\tnames: " ;
-        vector<string> tempname = readGate(file) ;
+        set<string> tempname = readGate(file) ;
+        vector<string> vectorName ;
         for ( auto i : tempname ) {
             cout << i << " " ;
+            vectorName.push_back(i) ;
         }
         cout << endl ;
 
-        string type = analyzeGate(file,tempname) ;
-        buildTree( type, tempname ) ;   // BuildTree
+        string type = analyzeGate(file,vectorName) ;
+        buildTree( type, vectorName ) ;   // BuildTree
     }
     else if ( type == ".end") {
         string temp = GetaToken( file ) ;
@@ -187,13 +198,13 @@ void analyzeLabel( string type, ifstream & file ) {
     }
 } // analyzeLabel
 
-vector<string> readGate(ifstream & file ) {
-    vector<string> temp ;
+set<string> readGate(ifstream & file ) {
+    set<string> temp ;
     string temp_string ;
     temp_string = PeekaToken(file) ;
     while( ! temp_string.empty() && temp_string[0] != '.' && ! IsNumber(temp_string[0]) ) {
         temp_string = GetaToken( file ) ;
-        temp.push_back(temp_string) ;
+        temp.insert(temp_string) ;
         temp_string = PeekaToken(file) ;
     }
     return temp ;
@@ -203,9 +214,9 @@ string analyzeGate(ifstream & file, vector<string> gate) {
     int gate_size = gate.size() ;
     string temp_String = GetaToken(file) ;
     string type ;
-    cout << temp_String << "\tFirst Gate" << endl ;
+    // cout << temp_String << "\tFirst Gate" << endl ;
     if (temp_String.find('-') != string::npos )  {
-        cout << "=====OR Gate=====" << endl ;
+        // cout << "=====OR Gate=====" << endl ;
         type = "or" ;
         GetaToken(file) ;
         for ( int i = 2 ; i < gate.size() ; i ++ ) {
@@ -214,12 +225,12 @@ string analyzeGate(ifstream & file, vector<string> gate) {
         }
     } // if
     else if ( temp_String.find('0') != string::npos) {
-        cout << "=====NOT Gate=====" << endl ;
+        // cout << "=====NOT Gate=====" << endl ;
         type = "NOT" ;
         GetaToken(file) ;
     } // else if
     else {
-        cout << "=====AND Gate=====" << endl ;
+        // cout << "=====AND Gate=====" << endl ;
         GetaToken(file) ;
         type = "AND" ;
     }
@@ -231,13 +242,13 @@ string analyzeGate(ifstream & file, vector<string> gate) {
 
 void buildTree( string type, vector<string> gate ) {
     string dest = gate.back() ;
-    if ( ! gGateVertex[dest] ) {
+    if ( ! gGateVertex[dest] ) { // 如果沒有destination的node
         DirectedGraph::vertex_descriptor v1 = boost::add_vertex(g);
         g[v1].name = dest ;
         g[v1].type = type ;
         gGateVertex[dest] = v1 ;
     }
-    else if ( gGateVertex[dest] && g[gGateVertex[dest]].type.empty()) {
+    else if ( gGateVertex[dest] && g[gGateVertex[dest]].type.empty()) { // 如果已經有destination的node 但是沒有type
         g[gGateVertex[dest]].type = type ;
     }
     else {
@@ -245,19 +256,31 @@ void buildTree( string type, vector<string> gate ) {
     } // else
     DirectedGraph::vertex_descriptor destVertex = gGateVertex[dest] ;
     gate.pop_back() ;
+    gGateInbound[dest] = gate ;
     for ( auto i : gate ) {
-        if ( gGateVertex[i] ) {
+        gGateStatus[i] = false ;
+        if ( gGateVertex[i] ) { // 如果已經有這個節點
             boost::add_edge(gGateVertex[i], destVertex, g ) ;
 
         } // if
-        else {
-            DirectedGraph::vertex_descriptor v1 = boost::add_vertex(g);
-            gGateVertex[i] = v1 ;
-            g[v1].name = i ;
-            g[v1].type = type ;
-            boost::add_edge(v1, destVertex, g) ;
+        else { // 如果沒有這個節點
+            if ( gInputs.find(i) == gInputs.end() ) {
+                DirectedGraph::vertex_descriptor v1 = boost::add_vertex(g);
+                gGateVertex[i] = v1 ;
+                g[v1].name = i ;
+                g[v1].type = type ;
+                boost::add_edge(v1, destVertex, g) ;
+            } // if
+            else {
+                bool edgeExists;
+                DirectedGraph::edge_descriptor  e;
+                boost::tie(e, edgeExists) = boost::edge(ghead, destVertex, g) ;
+                if (!edgeExists) { // 不要有重複的邊
+                    e = boost::add_edge(ghead, destVertex, g).first ;
+                }
+            }
         }
-    }
+    } // for
 } // buildTree
 
 bool checkResourceValid( string gate ) {
@@ -266,22 +289,64 @@ bool checkResourceValid( string gate ) {
     return false ;
 } // checkResourceValid
 
+void GateInputChange( queue<string> & eachqueue, VertexProperty resource ) {
+    if ( resource.type == "AND") {
+        eachqueue.push(resource.name) ;
+    } // if
+    else if (resource.type == "OR") {
+
+    }
+    else {
+
+    }
+} // GateInputChange
+
 int ListScheduling() {
     gandConstraint ;
     gorConstraint ;
     gnotConstraint ;
-    gGateInbound ;
-    // Change Status
-        // Check GateResource Valid from gGateStatus
-        // put ready Gate into readyqueue
+    
+    bool finish = false ;
+    int cycle = 0 ;
+    Vertex v ;
+    vector<bool> visited(boost::num_vertices(g), false);
+    unordered_map<Vertex,int> waitingqueue;
+    // visited[v] = true;
+    while( ! finish ) {
+        if ( cycle == 0 ) {
+            DirectedGraph::adjacency_iterator ai, ai_end;
+            cout << "ListScheduling test" << endl ;
+            for (boost::tie(ai, ai_end) = boost::adjacent_vertices(ghead, g); ai != ai_end; ++ai) {
+                if ( ! waitingqueue[*ai] ) {
+                    waitingqueue[*ai] = 1;
+                }
+                else {
+                    waitingqueue[*ai] ++ ;
+                }
 
-    // Check how many resource this cycle can use
+                // 待修改
+                if ( waitingqueue[*ai] == gGateInbound[g[*ai].name].size() ) {
+                    ORreadyqueue.push(g[*ai].name) ;
+                } // if
+                cout << g[*ai].name << endl ;
+            } // for
+        } // if
 
-    // get resource from readyqueue ( 'select' priority )
 
-    // run 1 cycle
+        // Change Status
+            // Check GateResource Valid from gGateStatus
+            // put ready Gate into readyqueue
 
-    // cycle ++
+        // Check how many resource this cycle can use
+
+        // get resource from readyqueue ( 'select' priority )
+
+        // run 1 cycle
+
+        cycle ++ ;
+    } // while
+
+    return cycle ;
 } // ListScheduling
 
 
@@ -365,6 +430,8 @@ int main(int argc, char const *argv[])
 
     }
     vertex_dfs() ;
+
+    ListScheduling() ;
     /* code */
     return 0;
 }
